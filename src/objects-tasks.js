320052214ab3b1466ccbdb21c438ac2c69b5e886
asharovatova <co.rin@mail.ru>
2025-02-19 18:27:01 +0300

    feat: implement from JSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -236,8 +236,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const resObj = JSON.parse(json);
+  Object.setPrototypeOf(resObj, proto);
+
+  return resObj;
 }
 
 /**
